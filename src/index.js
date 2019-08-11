bd2c05bc90c86152d506a85f3e2f46e329e05f8d
jacobworrel <jacobworrel@gmail.com>
2019-08-11 12:17:36 -0700

    complete auth chapter

--- src/index.js
+++ src/index.js
@@ -1,26 +1,25 @@
 const { GraphQLServer } = require('graphql-yoga');
 const { prisma } = require('./generated/prisma-client');
+const Query = require('./resolvers/Query')
+const Mutation = require('./resolvers/Mutation')
+const User = require('./resolvers/User')
+const Link = require('./resolvers/Link')
 
 const resolvers = {
-  Query: {
-    feed (root, args, context, info) {
-      return context.prisma.links();
-    },
-    info: () => 'This is the API of a Hackernews Clone',
-  },
-  Mutation: {
-    post (root, args, context) {
-      return context.prisma.createLink({
-        url: args.url,
-        description: args.description,
-      });
-    },
-  },
+  Query,
+  Mutation,
+  User,
+  Link,
 };
 
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
   resolvers,
-  context: { prisma },
+  context (request) {
+    return {
+      ...request,
+      prisma,
+    };
+  },
 });
 server.start(() => console.log(`Server is running on http://localhost:4000`));
